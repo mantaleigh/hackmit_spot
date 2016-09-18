6ba1d7b89da085b99a92eff2c2d9cbeba0613dda
mantaleigh <samantha@samanthavoigt.com>
2016-09-18 10:49:56 -0400

    final

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
-spot_available = False
-
-total_avail_spots = 5
-
-import os, random
+import os, random, datetime
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+spot_available = False
+initTimestamp = None
+
+hasVisitedParked = False
+
+defaultMeterTime = datetime.time(2, 0, 0, 0) # 2 hours
 
 app = Flask(__name__)
 
@@ -20,18 +23,26 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_very_secret_secret')
 @app.route('/')
 def search():
     """Render website's search page."""
-    return render_template('search.html', total_avail_spots=total_avail_spots)
-
+    global hasVisitedParked
+    hasVisitedParked = False
+    return render_template('search.html', showSearch=True)
 
 @app.route('/selected/')
 def selected():
     """Render the website's selected page."""
-    return render_template('selected.html')
+    return render_template('selected.html', showBackToMap=True)
 
 @app.route('/parked/')
 def parked():
     """Render the website's parked page."""
-    return render_template('parked.html')
+    global initTimestamp
+    global hasVisitedParked
+
+    if not hasVisitedParked:
+        initTimestamp = datetime.datetime.time(datetime.datetime.now())
+        hasVisitedParked = True
+
+    return render_template('parked.html', showBackToMap=True, time_remaining='2 hr 0 mins', meter_val='$0.00', time_used='0 MINUTES')
 
 @app.route('/availability_data', methods=['GET', 'POST'])
 def availability_data():
@@ -46,7 +57,42 @@ def get_availability():
     global spot_available
     return jsonify(spot_available)
 
+@app.route('/get_parking_updates')
+def get_parking_updates():
+    global initTimestamp
+    global hasVisitedParked
+    global defaultMeterTime
 
+
+    if initTimestamp:
+        currentTime = datetime.datetime.now()
+
+        # print "currentTime----> " + str(currentTime)
+        # print "defaultMeterTime----> " + str(defaultMeterTime)
+        # print "initTimestamp----> " + str(initTimestamp)
+
+        temp_delta = currentTime - datetime.datetime.combine(datetime.date.today(), initTimestamp)
+
+        # print temp_delta
+        # print datetime.datetime.combine(datetime.date.today(), defaultMeterTime) - temp_delta
+
+        timeString = '2 hr 0 mins'
+        
+
+        # timeString = delta.strftime('%I hr %M mins')
+
+        # datetime.combine(temp_delta, )
+
+
+
+    else: 
+        timeString = '2 hr 0 mins'
+
+
+
+
+
+    return jsonify({'time_left': timeString})
 
 ###
 # The functions below should be applicable to all Flask apps.
